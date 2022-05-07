bd22058781dfff482b467a0a97445ec8481d9fa8
AnantharamanK <heavywestwind@outlook.com>
2022-05-07 14:10:27 +0530

    Updated sample tc

--- Tests/sampletest.js
+++ Tests/sampletest.js
@@ -15,19 +15,33 @@ const capabilities = {
 
 async function runTestWithCaps() {
     let driver = new webdriver.Builder().withCapabilities({}).forBrowser("chrome").build();
+    await driver.manage().window().maximize();
     try {
         await driver.get(env.url);
-
         await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.home.btnLogin, 10000))).click();
         await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.loginScreen.txtEmail, 10000))).sendKeys(env.username);
         await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.loginScreen.txtPassword, 10000))).sendKeys(env.password);
         await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.loginScreen.btnLogin, 10000))).click();
+        //switch to frame using web element
+        await driver.sleep(15000);
+        //await driver.wait(webdriver.until.elementLocated(driver.findElement(screenObject.homeScreen.ifrHomeScreen, 20000)))
+        await driver.switchTo().frame(driver.findElement(screenObject.homeScreen.ifrHomeScreen));
 
+        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.homeScreen.btnaddaproperty, 15000))).then(ele => {
+            ele.click();
+        }).catch(async(ele) => {
+
+            await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.homeScreen.btnaddaproperty, 2000))).click();
+        })
+        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.txtPropertyAddress, 10000))).sendKeys("107 Esplanade , Rockingham WA 6168");
+        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.drpSaleType, 10000))).sendKeys("Private Treaty");
+        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.drpContractofSale, 10000))).sendKeys("No");
+        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.btnGetStarted, 10000))).click();
 
     } catch (e) {
         //marking the test as Failed if product has not been added to the cart
         console.log("Error:", e.message)
     }
-    await driver.quit();
+    // await driver.quit();
 }
 runTestWithCaps();
