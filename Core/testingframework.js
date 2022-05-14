34718ed1042508ad36170781587eb77003ebc3b4
AnantharamanK <heavywestwind@outlook.com>
2022-05-14 16:04:08 +0530

    framework code added

--- Core/testingframework.js
+++ Core/testingframework.js
@@ -1,45 +1,190 @@
 const webdriver = require('selenium-webdriver');
-const { By } = require('selenium-webdriver');
+const { By, until } = require('selenium-webdriver');
 const assert = require('assert');
 const env = require('../env.json');
 const screenObject = require('../ObjectRepository/ObjectRepo.json');
 var logger = require('logger');
 const { v4: uuidv4 } = require('uuid');
+const { WebElementPromise } = require('selenium-webdriver');
+const { promise } = require('selenium-webdriver');
+const { WebElement } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
+const chromium = require('selenium-webdriver/chromium');
+const edge = require('selenium-webdriver/edge');
+const ie = require('selenium-webdriver/ie');
+const { Timeouts } = require('selenium-webdriver/lib/capabilities');
+const { ServiceBuilder } = require('selenium-webdriver/chrome');
 
-// Input capabilities
-const capabilities = {
-    'device': 'iPhone 11',
-    'realMobile': 'true',
-    'os_version': '14.0',
-    'browserName': 'iphone',
-    'name': 'BStack-[NodeJS] Sample Test', // test name
-    'build': 'BStack Build Number 1' // CI/CD job or build name
-}
+
+
 module.exports.browser = class browser {
     constructor() {
         this.log = logger.createLogger('././TestReports/logs/' + uuidv4() + '.log'); // logs to a file
-        this.log.info('loading an array', [1, 2, 3], 'now!');
+        this.log.info('Started....');
 
     }
-    init() {
-        this.driver = new webdriver.Builder()
-            .withCapabilities({})
-            .forBrowser("chrome").build();
+    browserOptions(args = null) {
+
+
+        switch (env.browser) {
+            case "chrome":
+                this.options = new chrome.Options();
+                break;
+            case "chromium":
+                this.options = new chromium.Options();
+                break;
+            case "firefox":
+                this.options = new firefox.Options();
+                break;
+            case "ie":
+                this.options = new ie.Options();
+                break;
+            case "edge":
+                this.options = new edge.Options();
+                break;
+            default:
+                this.options = new chrome.Options();
+                break;
+        }
+
+        if (args == null)
+            return this.options;
+
+        this.options.addArguments(args);
+        return this.options;
+    }
+
+    async maximize() {
+        await this.driver.manage().window().maximize();
+    }
+
+    async init() {
+        this.log.info('init started')
+        try {
+            if (!this.options)
+                this.browserOptions();
+
+            switch (env.browser) {
+                case "chrome":
+                    this.driver = new webdriver.Builder().forBrowser("chrome").setChromeOptions(this.options).build();
+                    break;
+                case "chromium":
+                    this.driver = new webdriver.Builder().forBrowser("chromium").setChromeOptions(this.options).build();
+                    break;
+                case "firefox":
+                    this.driver = new webdriver.Builder().forBrowser("firefox").setFirefoxOptions(this.options).build();
+                    break;
+                case "ie":
+                    this.driver = new webdriver.Builder().forBrowser("ie").setIeOptions(this.options).build();
+                    break;
+                case "edge":
+                    this.driver = new webdriver.Builder().forBrowser("edge").setEdgeOptions(this.options).build();
+                    break;
+                default:
+                    this.driver = new webdriver.Builder().forBrowser("chrome").setChromeOptions(this.options).build();
+                    break;
+            }
+            await this.driver.manage().setTimeouts(env.timeout);
+        } catch (e) {
+            this.log.error('error in init', e);
+        }
+
     }
 
     async navigate(url) {
-        await driver.get(url);
+        try {
+            await this.driver.get(url);
+        } catch (e) {
+            this.log.error('error in navigate url', e);
+        }
+
     }
 
     async element(property) {
-        return driver.wait(webdriver.until.elementIsVisible(driver.findElement(property, (+env.elementwait * 1000)))).then(element => {
+        this.log.info('find ', property)
+        return this.driver.wait(
+            this.driver.until.elementIsVisible(
+                driver.findElement(property, env.elementwait))).then((element) => {
+            this.log.info('Element identified : ', property)
             return element;
-        }).catch(err => {
+        }, (err) => {
+            this.log.error('Error on identifying element : ', property)
             return err;
         })
     }
 
-    element(orPath) {
+    async click(property, waittime = env.elementwait) {
+        return this.driver.wait(until.elementIsVisible(this.driver.findElement(property, waittime))).then(ele => {
+            this.log.info('Element identified : ', property);
+            try {
+                ele.click();
+                return ele;
+            } catch (e) {
+                this.log.error('Error on clicking element : ', property, e)
+                return e;
+            }
+
+        }).catch(err => {
+            this.log.error('Error on identifying element : ', property, err)
+            return err;
+        })
+    }
+    async sendKeys(property, value, waittime = env.elementwait) {
+        return this.driver.wait(until.elementIsVisible(this.driver.findElement(property, waittime))).then(ele => {
+            this.log.info('Element identified : ', property);
+            try {
+                ele.sendKeys(value);
+                return ele;
+            } catch (e) {
+                this.log.error('Error on typing element : ', property, e)
+                return e;
+            }
+
+        }).catch(err => {
+            this.log.error('Error on identifying element : ', property, err)
+            return err;
+        })
+    }
+    async quit() {
+        return this.driver.quit().then(() => {
+            this.log.info('Driver closed successfully')
+            return true;
+        }).catch(e => {
+            this.log.info('Error closing driver', e)
+            return false;
+        });
+    }
+
+
+
+    SwitchDisplayedFrame = function(object) {
+        return new Promise(async(resolve, reject) => {
+
+            let iframe = await driver.findElements(object);
+            for (let index = 0; index < iframe.length; index++) {
+                const element = iframe[index];
+                element.isDisplayed().then(async(status) => {
+                    if (status) {
+                        console.logstatus, (new Date())
+                        await driver.switchTo().frame(element);
+                        resolve(status)
+                    }
+                }).catch(err => { console.log(err) });
+            }
+            reject(false);
+        });
+    }
+}
+
+
+module.exports.dvr = class dvr extends WebElementPromise {
+    constructor() {
 
     }
+
+    static jalabula(params) {
+
+    }
+
 }
--- Tests/sampletest.js
+++ Tests/sampletest.js
@@ -3,8 +3,10 @@ const { By } = require('selenium-webdriver');
 const assert = require('assert');
 const env = require('../env.json');
 const screenObject = require('../ObjectRepository/ObjectRepo.json');
-const web = require('../Core/testingframework.js')
-    // Input capabilities
+const web = require('../Core/testingframework.js');
+const chrome = require('selenium-webdriver/chrome');
+
+// Input capabilities
 const capabilities = {
     'device': 'iPhone 11',
     'realMobile': 'true',
@@ -15,36 +17,63 @@ const capabilities = {
 }
 
 async function runTestWithCaps() {
-    const web1 = new web.browser();
 
-    let driver = new webdriver.Builder().withCapabilities({}).forBrowser("chrome").build();
+
+    var options = new chrome.Options();
+    options.addArguments("--incognito");
+    options.addArguments("--disable-extensions");
+    let driver = new webdriver.Builder().forBrowser("chrome").setChromeOptions(options).build();
     await driver.manage().window().maximize();
-    try {
-        await driver.get(env.url);
-        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.home.btnLogin, 10000))).click();
-        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.loginScreen.txtEmail, 10000))).sendKeys(env.username);
-        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.loginScreen.txtPassword, 10000))).sendKeys(env.password);
-        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.loginScreen.btnLogin, 10000))).click();
-        //switch to frame using web element
-        await driver.sleep(15000);
-        //await driver.wait(webdriver.until.elementLocated(driver.findElement(screenObject.homeScreen.ifrHomeScreen, 20000)))
-        await driver.switchTo().frame(driver.findElement(screenObject.homeScreen.ifrHomeScreen));
-
-        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.homeScreen.btnaddaproperty, 15000))).then(ele => {
-            ele.click();
-        }).catch(async(ele) => {
-
-            await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.homeScreen.btnaddaproperty, 2000))).click();
-        })
-        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.txtPropertyAddress, 10000))).sendKeys("107 Esplanade , Rockingham WA 6168");
-        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.drpSaleType, 10000))).sendKeys("Private Treaty");
-        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.drpContractofSale, 10000))).sendKeys("No");
-        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.btnGetStarted, 10000))).click();
-
-    } catch (e) {
-        //marking the test as Failed if product has not been added to the cart
-        console.log("Error:", e.message)
+    await driver.get(env.url);
+    await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.home.btnLogin, 10000))).click();
+    await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.loginScreen.txtEmail, 10000))).sendKeys(env.username);
+    await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.loginScreen.txtPassword, 10000))).sendKeys(env.password);
+    await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.loginScreen.btnLogin, 10000))).click();
+    //switch to frame using web element
+    await driver.sleep(10000);
+    let ifrCnt = 0
+    while (ifrCnt < 20) {
+        console.log(new Date())
+        try {
+            driver.findElements(screenObject.homeScreen.ifrHomeScreen).then(iframe => {
+                if (iframe.length > 0) { ifrCnt = 20 }
+            })
+            ifrCnt = ifrCnt + 1
+            await driver.sleep(10000);
+        } catch (e) {
+            console.log("Error:", e.message)
+        }
     }
-    // await driver.quit();
+
+    await driver.sleep(4000);
+    await driver.wait(webdriver.until.elementIsEnabled(driver.findElement(screenObject.homeScreen.ifrHomeScreen, 20000)))
+    await driver.switchTo().frame(driver.findElement(screenObject.homeScreen.ifrHomeScreen));
+
+    await driver.wait(webdriver.until.elementIsEnabled(driver.findElement(screenObject.homeScreen.btnaddaproperty, 15000))).click();
+    await driver.sleep(4000);
+    await driver.switchTo().defaultContent();
+
+    await SwitchDisplayedFrame(screenObject.homeScreen.ifrHomeScreen);
+
+
+    await driver.wait(webdriver.until.elementLocated(driver.findElement(screenObject.addPropertyScreen.txtPropertyAddress, 10000))).sendKeys("107 Esplanade , Rockingham WA 6168");
+    await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.drpSaleType, 10000))).sendKeys("Private Treaty");
+    await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.drpContractofSale, 10000))).sendKeys("No");
+    await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.btnGetStarted, 10000))).click();
+
+    await driver.quit();
 }
-runTestWithCaps();
+
+async function frameworkCode() {
+    var driver = new web.browser();
+    driver.browserOptions(["--incognito", "--disable-extensions"])
+    await driver.init();
+    await driver.maximize();
+    await driver.navigate(env.url);
+    await driver.click(screenObject.home.btnLogin, 30000);
+    await driver.sendKeys(screenObject.loginScreen.txtEmail, env.username, 30000);
+    await driver.sendKeys(screenObject.loginScreen.txtPassword, env.password, 30000);
+    driver.click(screenObject.loginScreen.btnLogin);
+    //await driver.quit();
+}
+frameworkCode();
